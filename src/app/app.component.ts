9af0c7674005fbd57490f792a8814fc58eaef991
puneetkk <puneet.kankane@gmail.com>
2017-07-07 09:38:32 +0530

    First commit without services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Abc Insurance Self Service Portal';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { AddressComponent } from './address/address.component';
+import { PersonalDetailsComponent } from './personal-details/personal-details.component';
+import { ChangeAddressComponent } from './change-address/change-address.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register',      component: RegistrationComponent },
+  { path: 'personal-details',      component: PersonalDetailsComponent },
+  { path: 'change-address',      component: ChangeAddressComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+    AddressComponent,
+    PersonalDetailsComponent,
+    ChangeAddressComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
