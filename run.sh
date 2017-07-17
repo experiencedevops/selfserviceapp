e96d91f9918e0aff9d997b5278742d539a603131
puneetkk <puneet.kankane@gmail.com>
2017-07-17 22:44:07 +0530

    updated param

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # !/bin/sh
-/bin/sed -i "s|http://localhost:8080|$\{API_URL}|" /usr/share/nginx/html/main.bundle.js
+/bin/sed -i "s|http://localhost:8080|${API_URL}|" /usr/share/nginx/html/main.bundle.js
 
 nginx -g 'daemon off;'
